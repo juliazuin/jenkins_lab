66a2599c76a4b68b24224cfffea0aabe48995de5
juliazuin <zuinjulia@hotmail.com>
2021-10-22 21:19:37 +0000

    ajustes

--- exercicio/deploy.sh
+++ exercicio/deploy.sh
@@ -1,21 +1,21 @@
 !/bin/bash
 
 cd /workspace/jenkins_lab/exercicio/ex_terraform/terraform
-~/terraform init
-~/terraform apply -auto-approve
+/workspace/jenkins_lab/exercicio/ex_terraform/terraform init
+/workspace/jenkins_lab/exercicio/ex_terraform/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(~/workspace/jenkins_lab/exercicio/ex_terraform/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> /workspace/jenkins_lab/exercicio/ex_ansible/ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 sudo apt install ansible
 
-cd ../exercicio/ansible
+cd /workspace/jenkins_lab/exercicio/ex_ansible/ansible
 
 echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-sudo ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+sudo ansible-playbook -i /workspace/jenkins_lab/exercicio/ex_ansible/hosts /workspace/jenkins_lab/exercicio/ex_ansible/provisionar.yml
